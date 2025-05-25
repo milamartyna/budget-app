be4da724c3603d1c2076ef691384d907543f825b
Martyna Mila <martynamila2001@gmail.com>
2025-05-25 10:05:12 +0200

    fix: allow user to create transactions without category

--- frontend/src/api/TransactionService.ts
+++ frontend/src/api/TransactionService.ts
@@ -8,6 +8,6 @@ export const getTransactions = async(userName: string)=> {
     return response as Array<Transaction>;
 }
 
-export async function create_new_transaction(transaction: Transaction){
+export async function addTransaction(transaction: Transaction){
     return await http.post("/transactions", transaction);
 }
